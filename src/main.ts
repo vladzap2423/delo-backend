9f5d722b6e29f699e16fb45ac1b98fdda15c45dc
vladzap2423 <vladzap2423@mail.ru>
2025-09-16 22:54:54 +1200

    Вторник Дом

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: true,
     credentials: true,
     exposedHeaders: 'set-cookie',
   });
